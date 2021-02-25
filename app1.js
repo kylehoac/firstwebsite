7ec6df262a9fddf2ba19e4b1683229c3847ba0b6
‘KyleHoac’ <hoackyle@gmail.com>
2021-02-24 20:46:45 -0800

    added js

---
+++ app1.js
@@ -0,0 +1,3 @@
+let userName = prompt('Please enter your name');
+
+document.write('<h2>' + userName + '(</h2>');
---
+++ app3.js
@@ -0,0 +1,5 @@
+if (userName == 'Kyle'){
+    document.write('<h2>' + 'Goodbye Kyle' + '</h3>');
+} else {
+    document.write('<h2>' + 'Goodbye ' + userName + '</h3>');
+}
